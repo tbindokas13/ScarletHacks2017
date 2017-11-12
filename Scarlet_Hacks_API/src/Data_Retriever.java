d597b0306631f4898f70983db4c1bcb6c483021b
Pradeep Senthil <pradeepsen99@gmail.com>
2017-11-11 23:24:51 -0600

    Final Code

--- Scarlet_Hacks_API/src/Data_Retriever.java
+++ Scarlet_Hacks_API/src/Data_Retriever.java
@@ -1,15 +1,4 @@
-/**
- * 
- * @author Tomas Copyright (C) 2017 Pradeep Senthil and Tomas Bindokas. All rights reserved. This
- *         class belongs to the USAlmanac project.
- */
-public class Data_Retriever 
-{
-	String Data="";
-	public void GetZipCode(int ZipCode) 
-	{
-		Zipcode getZipCode = new Zipcode(ZipCode);
-		Data= getZipCode.urlToJSON();
-		System.out.println(Data);
-	}
+
+public class Data_Retriever {
+	
 }
--- Scarlet_Hacks_API/src/Zipcode.java
+++ Scarlet_Hacks_API/src/Zipcode.java
@@ -4,14 +4,18 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+
+import javax.swing.table.TableModel;
+
+import org.json.XML;
 
 /**
  * 
- * @author Pradeep Copyright (C) 2017 Pradeep Senthil and Tomas Bindokas. All rights reserved. 
- * This class belongs to the USAlmanac project.
+ * @author Pradeep Copyright (C) 2017 Pradeep Senthil and Tomas Bindokas. All
+ *         rights reserved. This class belongs to the USAlmanac project.
  */
-public class Zipcode
-{
+public class Zipcode {
 
 	/**
 	 * Stores the zipcode for instance of class created.
@@ -19,21 +23,35 @@ public class Zipcode
 	public int zipcode = 0;
 
 	/**
+	 * Stores the JSON string format.
+	 */
+	public String JSON = "";
+
+	/**
+	 * Splits the main string into an array seperated by new line.
+	 */
+	public String[] JSONarr;
+
+	/**
 	 * Constructor for zipcode class.
 	 * 
-	 * @param zip enters the zipcode into the application.
+	 * @param zip
+	 *            enters the zipcode into the application.
 	 */
 	public Zipcode(int zip) {
 		zipcode = zip;
+		urlToJSON();
+		JSONarr = JSON.split("\\r?\\n");
 	}
 
 	/**
-	 * This function takes the zipcode entered by the user and returns a string of the data in JSON format.
-	 * The data received form the database is in XML format, the function also converts XML to JSON.
+	 * This function takes the zipcode entered by the user and returns a string of
+	 * the data in JSON format. The data received form the database is in XML
+	 * format, the function also converts XML to JSON.
 	 * 
 	 * @return String JSON file retrieved form API Call.
 	 */
-	public String urlToJSON() {
+	public void urlToJSON() {
 		try {
 			String mainURL = "https://usgeocoder.com/api/get_info.php?zipcode=" + Integer.toString(zipcode)
 					+ "&authkey=f4bbfda269405c5a27e0a87bc6868b1a";
@@ -53,15 +71,91 @@ public class Zipcode
 				output += a + "\n";
 			}
 			conn.disconnect();
-			return XML.toJSONObject(output).toString(4);
+			JSON = XML.toJSONObject(output).toString(4);
 
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
-			return "404 ERROR";
+			JSON = "404 ERROR";
 		} catch (IOException e) {
 			e.printStackTrace();
-			return "404 ERROR";
+			JSON = "404 ERROR";
 		}
+	}
+
+	public String[] getSentateNum1() {
+		ArrayList<String> output = new ArrayList<String>();
+		for (int i = 0; i < JSONarr.length; i++) {
+			if (JSONarr[i].contains("national_senator_1")) {
+				String temp = JSONarr[i];
+				temp = temp.substring(temp.indexOf(" ", 19), temp.length() - 2);
+				temp = temp.substring(2, temp.length());
+				output.add(temp);
+			}
+		}
+		String[] stockArr = new String[output.size()];
+		stockArr = output.toArray(stockArr);
+
+		return stockArr;
+	}
+
+	public String[] getSentateNum2() {
+		ArrayList<String> output = new ArrayList<String>();
+		for (int i = 0; i < JSONarr.length; i++) {
+			if (JSONarr[i].contains("national_senator_2")) {
+				String temp = JSONarr[i];
+				temp = temp.substring(temp.indexOf(" ", 19), temp.length() - 2);
+				temp = temp.substring(2, temp.length());
+				output.add(temp);			}
+		}
+		String[] stockArr = new String[output.size()];
+		stockArr = output.toArray(stockArr);
+
+		return stockArr;
+	}
+
+	public String[] getRep() {
+		ArrayList<String> output = new ArrayList<String>();
+		for (int i = 0; i < JSONarr.length; i++) {
+			if (JSONarr[i].contains("representative_1")) {
+				String temp = JSONarr[i];
+				temp = temp.substring(temp.indexOf(" ", 19), temp.length() - 2);
+				temp = temp.substring(2, temp.length());
+				output.add(temp);			}
+		}
+		String[] stockArr = new String[output.size()];
+		stockArr = output.toArray(stockArr);
+
+		return stockArr;
+	}
+
+	public String[] getCounty() {
+		ArrayList<String> output = new ArrayList<String>();
+		for (int i = 0; i < JSONarr.length; i++) {
+			if (JSONarr[i].contains("county_name")) {
+				String temp = JSONarr[i];
+				temp = temp.substring(temp.indexOf(" ", 19), temp.length()-1);
+				temp = temp.substring(2, temp.length());
+				output.add(temp);			}
+		}
+		String[] stockArr = new String[output.size()];
+		stockArr = output.toArray(stockArr);
+
+		return stockArr;
+	}
+	
+	public String[] getGov() {
+		ArrayList<String> output = new ArrayList<String>();
+		for (int i = 0; i < JSONarr.length; i++) {
+			if (JSONarr[i].contains("\"governor")) {
+				String temp = JSONarr[i];
+				temp = temp.substring(temp.indexOf(" ", 19), temp.length() - 2);
+				temp = temp.substring(2, temp.length());
+				output.add(temp);			}
+		}
+		String[] stockArr = new String[output.size()];
+		stockArr = output.toArray(stockArr);
+
+		return stockArr;
 	}
 
 }
